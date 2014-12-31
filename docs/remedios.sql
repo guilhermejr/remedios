bf2d11079893cb69769baa2d0249ba779b388cb0
Guilherme Jr <falecom@guilhermejr.net>
2014-12-31 10:10:02 -0300

    Adicionado a tabela de configurações no banco de dados

--- docs/remedios.sql
+++ docs/remedios.sql
@@ -1,10 +1,24 @@
+CREATE TABLE IF NOT EXISTS `configuracoes` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `vencido` char(1) CHARACTER SET latin1 COLLATE latin1_bin NOT NULL DEFAULT '1',
+  `avencer` char(1) NOT NULL DEFAULT '1',
+  `dias` int(11) NOT NULL DEFAULT 30,
+  `periodicidade` enum('dia','semana','mes') NOT NULL DEFAULT 'dia',
+  `usuario_id` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_configuracoes_usuarios1_idx` (`usuario_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+INSERT INTO `configuracoes` (`id`, `vencido`, `avencer`, `dias`, `periodicidade`, `usuario_id`) VALUES
+(1, '1', '1', 30, 'dia', 1);
+
 CREATE TABLE IF NOT EXISTS `indicacoes` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `descricao` varchar(255) NOT NULL,
   `usuario_id` int(10) unsigned NOT NULL,
   PRIMARY KEY (`id`),
   KEY `fk_indicacoes_usuarios1_idx` (`usuario_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=23 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 CREATE TABLE IF NOT EXISTS `indicacoes_remedios` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -13,7 +27,7 @@ CREATE TABLE IF NOT EXISTS `indicacoes_remedios` (
   PRIMARY KEY (`id`,`indicacao_id`,`remedio_id`),
   KEY `fk_indicacoes_has_remedios_remedios1_idx` (`remedio_id`),
   KEY `fk_indicacoes_has_remedios_indicacoes1_idx` (`indicacao_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=60 ;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 CREATE TABLE IF NOT EXISTS `remedios` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
@@ -26,7 +40,7 @@ CREATE TABLE IF NOT EXISTS `remedios` (
   `usuario_id` int(10) unsigned NOT NULL,
   PRIMARY KEY (`id`),
   KEY `fk_remedios_usuarios_idx` (`usuario_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=23 ;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
 
 CREATE TABLE IF NOT EXISTS `senhas` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
@@ -48,10 +62,13 @@ CREATE TABLE IF NOT EXISTS `usuarios` (
   `created` datetime NOT NULL,
   `updated` datetime NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=8 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
 
 INSERT INTO `usuarios` (`id`, `nome`, `email`, `senha`, `ativo`, `ultimoAcesso`, `created`, `updated`) VALUES
-(7, 'Guilherme Jr.', 'falecom@guilhermejr.net', '848d3ee0d1ac67f1f78c1b43e8001a7a6fae1050', '1', '2014-09-13 10:43:16', '2014-08-18 16:38:15', '2014-09-13 10:43:16');
+(1, 'Guilherme Jr.', 'falecom@guilhermejr.net', '848d3ee0d1ac67f1f78c1b43e8001a7a6fae1050', '1', '2014-12-31 10:46:33', '2014-08-18 16:38:15', '2014-12-31 10:46:33');
+
+ALTER TABLE `configuracoes`
+  ADD CONSTRAINT `fk_configuracoes_usuarios1` FOREIGN KEY (`usuario_id`) REFERENCES `usuarios` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 ALTER TABLE `indicacoes`
   ADD CONSTRAINT `fk_indicacoes_usuarios1` FOREIGN KEY (`usuario_id`) REFERENCES `usuarios` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
